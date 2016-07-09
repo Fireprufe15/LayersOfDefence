65afc0bf14bcaf59cef7fbfbe9caf03a18c938cb
stormmuller <bboystorm.mm@gmail.com>
2016-07-09 02:16:42 +0200

    Spawns stuff

---
+++ LayersOfDefence/Assets/Scripts/Nav.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Nav : MonoBehaviour {
+
+    [HideInInspector]
+    public List<Vector3> points;
+    [HideInInspector]
+    public bool isNavigating;
+
+    Rigidbody playerRB;
+    int currentPoint, lastPoint;
+    float speed = 5f;
+
+    bool isMoving = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // leave here
+        currentPoint = 0;
+        lastPoint = points.Count;
+        playerRB = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        if (!isNavigating) return;
+
+        if (new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y), Mathf.Round(transform.position.z))  == points[currentPoint])
+        {
+            if (!(currentPoint + 1 > points.Count - 1))
+            {
+                currentPoint++;
+            }
+            else
+            {
+                isNavigating = false;
+                playerRB.velocity = Vector3.zero;
+                return;
+            }
+        }
+
+        // Navigate
+        Vector3 direction = (points[currentPoint] - transform.position).normalized;
+        playerRB.velocity = direction * speed;
+    }
+}
---
+++ LayersOfDefence/Assets/Scripts/SpawnCreeps.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SpawnCreeps : MonoBehaviour {
+
+    public int creepsPerWave;
+    public float timeBetweenCreeps;
+    public float timeBetweenWaves;
+    public GameObject creep;
+    public MapSpawn mapSpawn;
+
+    [HideInInspector]
+    public int wave;
+    float nextCreepSpawn = 0f;
+    float nextWaveSpawn;
+    int creepCount = 0;
+    Vector3 StartingTile;
+    
+	void Start ()
+    {
+        StartingTile = mapSpawn.fullPath[0];
+	}
+
+	void Update ()
+    {
+        SpawnWave();
+	}
+
+    private void SpawnWave()
+    {
+        if (Time.timeSinceLevelLoad >= nextWaveSpawn)
+        {
+            if (Time.timeSinceLevelLoad >= nextCreepSpawn)
+            {
+                // Spawn creep
+                GameObject spawnedCreep = (GameObject)Instantiate(creep, StartingTile, Quaternion.identity);
+                Nav n = spawnedCreep.GetComponent<Nav>();
+                n.points = mapSpawn.fullPath;
+                n.isNavigating = true;
+
+                creepCount++;
+                nextCreepSpawn = Time.timeSinceLevelLoad + timeBetweenCreeps;
+
+                if (creepCount >= creepsPerWave)
+                {
+                    creepCount = 0;
+                    nextWaveSpawn = Time.timeSinceLevelLoad + timeBetweenWaves;
+                    wave++;
+                }
+            }
+        }
+    }
+}
