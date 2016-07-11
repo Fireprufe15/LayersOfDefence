9844e605c90d769a46c147fcf347e2290988d406
stormmuller <bboystorm.mm@gmail.com>
2016-07-11 14:26:51 +0200

    lklklklk

--- LayersOfDefence/Assets/Scripts/Menu.cs
+++ LayersOfDefence/Assets/Scripts/Menu.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour {
 
     public GameObject CreditsScreen;
+    public GameObject BuildTowerCanvas;
 
     public void LoadGame()
     {
-        SceneManager.LoadScene(1);
+        // SceneManager.LoadScene(1);
+        BuildTowerCanvas.SetActive(true);
+        gameObject.SetActive(false);
+        CreditsScreen.SetActive(false);
     }
 
     public void ShowCredits()
